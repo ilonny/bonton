86e810ccfb12a5a6670c38ae0e997103a5281eb4
ilonny <lonnyfox@bk.ru>
2020-05-07 20:20:07 +0300

    fonts

--- frontend/react-app/src/global-styles.js
+++ frontend/react-app/src/global-styles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import { Color } from "./lib/theme";
+import './assets/fonts/stylesheet.css';
 function fontFace(name, src, fontWeight = "normal", fontStyle = "normal") {
     return `
     @font-face{
@@ -18,13 +19,15 @@ function fontFace(name, src, fontWeight = "normal", fontStyle = "normal") {
     }
 `;
 }
+// ${fontFace("Futura", "FuturaPT-Light", 100, "normal")}
+// ${fontFace("Futura", "FuturaPT-Book", 300, "normal")}
+// ${fontFace("Futura", "FuturaPT-Book", "normal", "normal")}
+// ${fontFace("Futura", "FuturaPT-Bold", 600, "normal")}
+// ${fontFace("Futura", "futura_pt_book", "normal", "normal")}
 export const GlobalStyles = createGlobalStyle`
-  ${fontFace("FuturaPt", "FuturaPT-Light", 100, "normal")}
-  ${fontFace("FuturaPt", "FuturaPT-Book", 300, "normal")}
-  ${fontFace("FuturaPt", "FuturaPT-Medium", "normal", "normal")}
-  ${fontFace("FuturaPt", "FuturaPT-Bold", 600, "normal")}
   body {
-    font-family: "Helvetica", "FuturaPt", "Open Sans", sans-serif;
+    font-family: "Futura PT";
+    // font-family: "Helvetica", "FuturaPt", "Open Sans", sans-serif;
     height: 100vh;
     overflow: hidden;
     -webkit-font-smoothing: antialiased;
