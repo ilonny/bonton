8e6118c40eb950f7ac2ffadf099c5ae28b0c4a9f
Dmitry Gornak <lonnyfox@bk.ru>
2020-05-26 22:20:10 +0300

    minifix

--- frontend/react-app/src/features/sidebar/organisms/menu-category-link.js
+++ frontend/react-app/src/features/sidebar/organisms/menu-category-link.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const MenuCategoryLink = (props) => {
     const { href, toggleSideBar } = props;
     return (
-        <Link to={href ? href : ''} onClick={() => toggleSideBar()}>
+        <Link to={href ? href : ''} onClick={() => toggleSideBar ? toggleSideBar() : null}>
             <MenuCategory {...props} simple={true} />
         </Link>
     );
