40dda770a2b0b9d283401dff6b5f19c5be9c9915
Dmitry Gornak <lonnyfox@bk.ru>
2020-05-29 01:56:05 +0300

    product create js styling

--- backend/views/product/create.php
+++ backend/views/product/create.php
@@ -5,6 +5,8 @@ use yii\widgets\ActiveForm;
 use backend\models\Category;
 use backend\models\Size;
 use backend\models\Product;
+use kartik\file\FileInput;
+use kartik\select2\Select2
 
 ?>
 
@@ -20,9 +22,38 @@ use backend\models\Product;
             <?= $form->field($model, 'description')->textInput()->label('Короткое описание (не обязательно)'); ?>
             <?= $form->field($model, 'category_id')->hiddenInput(['value' => $category_id])->label(false); ?>
             <?= $form->field($model, 'price')->textInput()->label('Цена в рублях'); ?>
-            <?= $form->field($model, 'photos[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
-            <?= $form->field($model, 'size')->dropDownList($size_arr, ['multiple'=>'true']); ?>
-            <?= $form->field($model, 'color')->dropDownList($color_arr, ['multiple'=>'true']); ?>
+            <?= $form->field($model, 'photos[]')->widget(FileInput::classname(), [
+                    'options' => [
+                        'accept' => 'image/*',
+                        'multiple' => true,
+                    ],
+                    'pluginOptions' => [
+                        'showPreview' => true,
+                        'showCaption' => true,
+                        'showRemove' => true,
+                        'showUpload' => false
+                    ]
+                ]);
+            // ->fileInput(['multiple' => true, 'accept' => 'image/*']);
+            ?>
+            <?= $form->field($model, 'size')->widget(Select2::classname(), [
+                    'data' => $size_arr,
+                    'options' => ['placeholder' => 'Выберите доступные размеры товара'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);
+                // ->dropDownList($size_arr, ['multiple'=>'true']);
+            ?>
+            <?= $form->field($model, 'color')->widget(Select2::classname(), [
+                    'data' => $color_arr,
+                    'options' => ['placeholder' => 'Выберите доступные цвета товара'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]);
+                //->dropDownList($color_arr, ['multiple'=>'true']);
+            ?>
             <div class="form-group">
                 <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
             </div>
